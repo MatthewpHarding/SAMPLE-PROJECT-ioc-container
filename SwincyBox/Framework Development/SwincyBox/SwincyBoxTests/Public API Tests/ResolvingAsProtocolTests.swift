c45e9bd025936ec120eff34a7eb6de1b2147929a
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 13:25:57 +0100

    Removed New Lines And Comments
    We removed some new lines and removed comments which added no value.

--- SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/ResolvingAsProtocolTests.swift
+++ SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/ResolvingAsProtocolTests.swift
@@ -15,10 +15,7 @@ class ResolvingAsProtocolTests: XCTestCase {
     private let box = Box()
 
     override func setUpWithError() throws {
-        
-        // NOTICE that we are registering as the Protocol
         box.register() { Engine(make: "BMW", model: "Sport 5000", gears: 7) as Transmission }
-        
         box.register() { r in
             return Car(make: "BMW", model: "i8 Roadster Donington Grey", topSpeed: 120, doors: 2, imageName: "BMW-Roadster", engine: r.resolve()) as Vehicle
         }
@@ -31,10 +28,7 @@ class ResolvingAsProtocolTests: XCTestCase {
     // MARK: - Unit Tests
     
     func testRecievedVehiclePropertyValues() throws {
-        
-        // NOTICE that we are resolving as the Protocol
         let vehicle = box.resolve() as Vehicle
         XCTAssertTrue(Car.self == type(of: vehicle), "Unexpected type returned from box")
     }
-    
 }
