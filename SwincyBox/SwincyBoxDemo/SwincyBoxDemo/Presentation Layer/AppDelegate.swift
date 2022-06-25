125baa479fc0d6ab0b034a54b25bbdf290e868a6
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 19:57:19 +0100

    Removed New Lines
    Removed some unnecessary new lines which added no real value. By removing the empty new lines we also have condensed the code and are also keeping to our code convention.

--- SwincyBox/SwincyBoxDemo/SwincyBoxDemo/Presentation Layer/AppDelegate.swift
+++ SwincyBox/SwincyBoxDemo/SwincyBoxDemo/Presentation Layer/AppDelegate.swift
@@ -11,23 +11,16 @@ import SwincyBox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         // 📦 setup and register dependencies from inside your own SwincyBox-Config file
         App.shared.registerRootDependencies()
-        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
-    }
-
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 }
