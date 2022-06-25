f521233d581be67bfa27cf304be9d35acd0474d8
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 19:10:21 +0100

    Removed New Lines
    Removed some unnecessary new lines which added no real value. By removing the empty new lines we also have condensed the code and are also keeping to our code convention.

--- SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/Transient Storage/BasicTests_Transient.swift
+++ SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/Transient Storage/BasicTests_Transient.swift
@@ -9,9 +9,7 @@ import XCTest
 @testable import SwincyBox
 
 class BasicTests_Transient: XCTestCase {
-
     // MARK: - Setup / Tear Down
-    
     private let box = Box()
 
     override func setUpWithError() throws {
@@ -26,7 +24,6 @@ class BasicTests_Transient: XCTestCase {
     }
 
     // MARK: - Unit Tests
-    
     func testRecievedVehiclePropertyValues() throws {
         let vehicle = box.resolve() as Vehicle
         XCTAssertTrue("BMW" == vehicle.make, "Unexpected value from boxed protocol")
