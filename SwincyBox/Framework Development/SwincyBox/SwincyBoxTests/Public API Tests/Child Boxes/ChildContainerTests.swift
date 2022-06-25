0195cc4a67ece26829b2ade9f9ed68d5136850be
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 18:45:41 +0100

    Removed New Lines
    Removed some unnecessary new lines which added no real value. By removing the empty new lines we also have condensed the code and are also keeping to our code convention.

--- SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/Child Boxes/ChildContainerTests.swift
+++ SwincyBox/Framework Development/SwincyBox/SwincyBoxTests/Public API Tests/Child Boxes/ChildContainerTests.swift
@@ -15,7 +15,6 @@ class ChildContainerTests: XCTestCase {
     var mercedesBox: Box!
     
     override func setUpWithError() throws {
-        
         /*
             Scenario: Both manufacturers use the same engine manufacturer, Volkswagen.
          
@@ -47,32 +46,23 @@ class ChildContainerTests: XCTestCase {
                 c) Child box B (MERC) WILL contain a Showroom service
          
          */
-        
         rootBox.register(Transmission.self) {
-            
             // 📦 We can register this Engine as the 'Transmission' protocol resulting in ALL Transmission dependencies becoming VW Engine Models
-            
             return Engine(make: "VW", model: "White-Label Engine v5.0", gears: 6)
         }
-        
-        
         bmwBox = rootBox.addChildBox(forKey: "bmwBox")
         setUpBMWCars(withChildBox: bmwBox)
-        
         mercedesBox = rootBox.addChildBox(forKey: "mercedesBox")
         setUpMercedesCars(withChildBox: mercedesBox)
     }
     
     func setUpBMWCars(withChildBox box: Box) {
-        
         box.register(key: "sportsCar") { r in
             return Car(make: "BMW", model: "i8 Roadster Donington Grey", topSpeed: 120, doors: 2, imageName: "BMW-Roadster", engine: r.resolve()) as Vehicle
         }
-        
         box.register(key: "familyCar") { r in
             return Car(make: "BMW", model: "X3", topSpeed: 90, doors: 5, imageName: "BMW-X3", engine: r.resolve()) as Vehicle
         }
-        
         box.register() { r in
             return Showroom(familyCar: r.resolve(key: "familyCar"), sportsCar: r.resolve(key: "sportsCar"), manufacturer: "BMW") as Showroom
         }
@@ -82,11 +72,9 @@ class ChildContainerTests: XCTestCase {
         box.register(key: "sportsCar") { r in
             return Car(make: "Mercedes", model: "AMG GT Coupé", topSpeed: 160, doors: 3, imageName: "SportsCar", engine: r.resolve()) as Vehicle
         }
-        
         box.register(key: "familyCar") { r in
             return Car(make: "Mercedes", model: "C-Class Sedan", topSpeed: 100, doors: 5, imageName: "FamilyCar", engine: r.resolve()) as Vehicle
         }
-        
         box.register() { r in
             return Showroom(familyCar: r.resolve(key: "familyCar"), sportsCar: r.resolve(key: "sportsCar"), manufacturer: "Mercedes") as Showroom
         }
@@ -100,24 +88,20 @@ class ChildContainerTests: XCTestCase {
     
     func testThatBMWUseVWEngines() throws {
         let box: Box = bmwBox
-        
         let showroom = box.resolve() as Showroom
         let familyCar = showroom.familyCar
         let sportsCar = showroom.sportsCar
         XCTAssertTrue("BMW" == familyCar.make, "Unexpected make of car")
         XCTAssertTrue("BMW" == sportsCar.make, "Unexpected make of car")
-        
         XCTAssertTrue("VW" == familyCar.engine.make, "Unexpected make of engine")
         XCTAssertTrue("VW" == sportsCar.engine.make, "Unexpected make of engine")
     }
     
     func testThatBMWShowroomContainsBMWCars() throws {
         let box: Box = bmwBox
-        
         let showroom = box.resolve() as Showroom
         let familyCar = showroom.familyCar
         let sportsCar = showroom.sportsCar
-        
         XCTAssertTrue("BMW" == familyCar.make, "Unexpected make of car")
         XCTAssertTrue("BMW" == sportsCar.make, "Unexpected make of car")
     }
@@ -126,24 +110,20 @@ class ChildContainerTests: XCTestCase {
     
     func testThatMercedesUseVWEngines() throws {
         let box: Box = mercedesBox
-        
         let showroom = box.resolve() as Showroom
         let familyCar = showroom.familyCar
         let sportsCar = showroom.sportsCar
         XCTAssertTrue("Mercedes" == familyCar.make, "Unexpected make of car")
         XCTAssertTrue("Mercedes" == sportsCar.make, "Unexpected make of car")
-        
         XCTAssertTrue("VW" == familyCar.engine.make, "Unexpected make of engine")
         XCTAssertTrue("VW" == sportsCar.engine.make, "Unexpected make of engine")
     }
     
     func testThatMercedesShowroomContainsMercedesCars() throws {
         let box: Box = mercedesBox
-        
         let showroom = box.resolve() as Showroom
         let familyCar = showroom.familyCar
         let sportsCar = showroom.sportsCar
-        
         XCTAssertTrue("Mercedes" == familyCar.make, "Unexpected make of car")
         XCTAssertTrue("Mercedes" == sportsCar.make, "Unexpected make of car")
     }
