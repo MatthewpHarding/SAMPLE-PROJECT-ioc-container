3acc94e80510b3e51b493e6182051fd1f598ddd0
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 09:11:53 +0100

    Xcode Workspace File Removed
    The .xcworkspace file was opening in an empty state without displaying the two associated Xcode projects. For this reason, we have removed the workspace.
    To maintain a 'working state' for all pushed commits we are also embedding the Framework file to the sample project which was previously dynamically generated.

---
+++ SwincyBox/Framework Development/SwincyBoxDemo/SwincyBox.xcframework/ios-arm64/SwincyBox.framework/Headers/SwincyBox.h
@@ -0,0 +1,18 @@
+//
+//  Swincy.h
+//  Swincy
+//
+//  Created by Matthew Paul Harding on 15/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swincy.
+FOUNDATION_EXPORT double SwincyVersionNumber;
+
+//! Project version string for Swincy.
+FOUNDATION_EXPORT const unsigned char SwincyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swincy/PublicHeader.h>
+
+
