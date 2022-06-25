013cb92544e0c4b928fe4d41eec7cf97b5c3d158
Matthew Harding <MatthewpHarding@users.noreply.github.com>
2022-06-25 20:51:58 +0100

    Method Documentation
    We have added documentation to each type and method within the LifeType.swift file

--- SwincyBox/Framework Development/SwincyBox/SwincyBox/IOC Container/LifeType.swift
+++ SwincyBox/Framework Development/SwincyBox/SwincyBox/IOC Container/LifeType.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// LifeType represents the two states of dependency registration for class instances (reference types) such as, registering a creation factory or instantiating just one single instance. The transient type will register a factory generating new instances with each method call. The permanent type will create a single instance storing it immediately within memory for all future use. These two options do not apply to value types such as structs, which will always return copied data due to the nature of value types and areas of memory.
 public enum LifeType {
     case transient
     case permanent
